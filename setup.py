598f6ef779687d0edfb96f625d2bd66751d47554
Osc44r <oski90091@gmail.com>
2024-10-24 11:25:27 +0200

    Initialized library

---
+++ setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+import pathlib
+
+path = pathlib.Path(__file__).parent
+
+README = (path / "README.md").read_text()
+
+setup(
+    name="tf2schema",
+    version="0.0.1",
+    description="A Python package to interact with the Team Fortress 2 Schema",
+    long_description=README,
+    long_description_content_type="text/markdown",
+    author="Osc44r",
+    url="https://github.com/Osc44r/tf2schema/",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    keywords=[
+        "Team Fortress 2",
+        "TF2",
+        "Steam",
+        "Schema",
+        "API",
+    ]
+)
+
